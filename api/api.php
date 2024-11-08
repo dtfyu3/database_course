2ad79f8a7f05dee55c1c07783f693483f60be670
bogdanovM <maxkrasnodar3@gmail.com>
2024-11-08 19:00:58 +0300

    update reacord function added

--- api/api.php
+++ api/api.php
@@ -48,6 +48,7 @@ function getSubjects($user_id)
         $response['error'] = $e->getMessage();
     }
     $response['subjects'] = $subjects;
+    $conn->close();
     echo json_encode($response);
 }
 
@@ -71,6 +72,7 @@ function getSubjectGroups($teacher_id, $subject_id)
     } catch (Exception $e) {
         $response['error'] = $e->getMessage();
     }
+    $conn->close();
     echo json_encode($response);
 }
 function getJournal($teacher_id, $subject_id, $group_id, $type)
@@ -146,6 +148,7 @@ function getJournal($teacher_id, $subject_id, $group_id, $type)
     } catch (Exception $e) {
         $response['error'] = $e->getMessage();
     }
+    $conn->close();
     echo json_encode($response);
 }
 
@@ -170,6 +173,7 @@ function getGroupsForSubject($teacher_id, $subject)
     } catch (Exception $e) {
         $response['error'] = $e->getMessage();
     }
+    $conn->close();
     echo json_encode($response);
 }
 
@@ -194,8 +198,7 @@ function getAvg($group_id, $subject, $type, $begin_date = null, $end_date = null
                 $stmt = $conn->prepare("SELECT s.FIO, DATE_FORMAT(c.date, '%Y-%m') AS month, COALESCE(AVG(j.grade), 0) AS avg FROM students s LEFT JOIN grades_journal j ON s.id = j.student_id LEFT JOIN classes c ON j.class_id = c.id
                 LEFT JOIN subjects su ON c.subject_id = su.id WHERE s.group_id = ? AND su.name = ? AND c.date BETWEEN ? AND ? GROUP BY s.FIO, DATE_FORMAT(c.date, '%Y-%m') ORDER BY s.FIO, month;");
                 $stmt->bind_param('ssss', $group_id, $subject, $begin_date, $end_date);
-            }
-            else{
+            } else {
                 $stmt = $conn->prepare('select FIO, COALESCE(avg(j.grade),0) as avg from students s left join grades_journal j on s.id = j.student_id left join classes c on j.class_id = c.id left join subjects su on c.subject_id = su.id
                 where s.group_id = ? and (su.name = ? or c.subject_id IS NULL) group by FIO ');
                 $stmt->bind_param('ss', $group_id, $subject);
@@ -208,7 +211,7 @@ function getAvg($group_id, $subject, $type, $begin_date = null, $end_date = null
             while ($row = $result->fetch_assoc()) {
                 $avg[] = [
                     'month' => $row['month'],
-                    ($type == 'group'? 'group':'FIO') => ($type == 'group' ? $row['group_id'] : $row['FIO']),
+                    ($type == 'group' ? 'group' : 'FIO') => ($type == 'group' ? $row['group_id'] : $row['FIO']),
                     'avg' => $row['avg']
                 ];
             }
@@ -225,8 +228,59 @@ function getAvg($group_id, $subject, $type, $begin_date = null, $end_date = null
     } catch (Exception $e) {
         $response['error'] = $e->getMessage();
     }
+    $conn->close();
     echo json_encode($response);
 }
+
+function updateRecord($student, $subject_id, $date, $type, $value, $remark)
+{
+    $conn = getDbConnection();
+    $response = ['success' => false];
+    $conn->begin_transaction();
+    try {
+        if ($type == 'grades') {
+            $stmt = $conn->prepare('select j.id from grades_journal j join students s on j.student_id = s.id join classes c on j.class_id = c.id where s.FIO = ? and c.date = ? and c.subject_id = ?');
+            $stmt->bind_param('ssi', $student, $date, $subject_id);
+            $stmt->execute();
+            $result = $stmt->get_result()->fetch_column();
+            if ($result != null) {
+                if ($value == '-') {
+                    $sql = "delete from $type";
+                    $sql = $sql . '_journal where id = ?';
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bind_param('i', $result);
+                    $stmt->execute();
+                } else {
+                    $sql = "update $type";
+                    $sql = $sql . '_journal set grade = ? where id = ?';
+                    $stmt = $conn->prepare($sql);
+                    $stmt->bind_param('ii', $value, $result);
+                    $stmt->execute();
+                }
+            } else {
+                $stmt = $conn->prepare('select c.id from classes c where c.date = ? and c.subject_id = ?');
+                $stmt->bind_param('si', $date, $subject_id);
+                $stmt->execute();
+                $class_id = $stmt->get_result()->fetch_column();
+                $stmt = $conn->prepare('select s.id from students s where s.FIO = ?');
+                $stmt->bind_param('s', $student);
+                $stmt->execute();
+                $student_id = $stmt->get_result()->fetch_column();
+                $stmt = $conn->prepare('insert into grades_journal (type, grade, class_id, student_id) values ("",?,?,?)');
+                $stmt->bind_param('iii', $value, $class_id, $student_id);
+                $stmt->execute();
+            }
+        }
+    } catch (Exception $e) {
+        $conn->rollback();
+        $response['error'] = $e->getMessage();
+    }
+    $response['success'] = true;
+    $conn->commit();
+    $conn->close();
+    echo json_encode($response);
+}
+
 $data = json_decode(file_get_contents('php://input'), true);
 $action = null;
 $get_action = null;
@@ -235,10 +289,13 @@ if ($get_action != null) {
     if ($get_action == 'getSubjects') getSubjects($data['user_id']);
     elseif ($get_action == 'checkAuth') checkAuth($data['teacher_id'], $data['user_id']);
     elseif ($get_action == 'getGroupsForSubject') getGroupsForSubject($data['teacher_id'], $data['subject']);
-    elseif ($get_action == 'getAvg') getAvg($data['group_id'], $data['subject'], $data['type'],$data['begin_date'],$data['end_date']);
+    elseif ($get_action == 'getAvg') getAvg($data['group_id'], $data['subject'], $data['type'], $data['begin_date'], $data['end_date']);
+    elseif ($get_action == 'updateRecord') updateRecord($data['student'], $data['subject_id'], $data['date'], $data['type'], $data['value'], isset($data['remark']) ? $data['remark'] : null);
     elseif ($get_action == 'getSubjectGroups') {
         if (isset($data['teacher_id']) && isset($data['subject_id']))
             getSubjectGroups($data['teacher_id'], $data['subject_id']);
         else return http_response_code(403);
     } elseif ($get_action == 'getJournal') if (isset($data['teacher_id']) && isset($data['type'])) getJournal($data['teacher_id'], $data['subject_id'], $data['group_id'], $data['type']);
+
+    else echo ('Invalid action');
 }
