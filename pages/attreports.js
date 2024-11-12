dc80a14360d5356371402fa319b4187a1634769b
bogdanovM <maxkrasnodar3@gmail.com>
2024-11-12 12:10:39 +0300

    calling getSubjects from another script

--- pages/attreports.js
+++ pages/attreports.js
@@ -8,42 +8,12 @@ document.addEventListener("DOMContentLoaded", () => {
         return;
     }
     const subjects_list = document.getElementById('subjects-list');
-    let avgChart;
-    getSubjects();
-
-    function getSubjects() {
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', '../api/api.php?get_action=getSubjects', true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(JSON.stringify({
-            user_id: userId
-        }));
-
-        xhr.onload = function () {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                try {
-                    const response = JSON.parse(xhr.response);
-                    subjects = response['subjects'];
-                    fillList(subjects_list, subjects, 'name');
-                }
-                catch (e) { console.error('Error parsing JSON: ', e); }
-            }
-        }
-    }
-
-    function fillList(list, values, name) {
-        if (name === 'group' && list.options.length > 1) {
-            while (list.options.length > 1) list.remove(1);
-        }
-        for (const subject of Object.values(values)) {
-            const option = document.createElement("option");
-            option.textContent = subject[name];
-            list.appendChild(option);
-        }
-    }
+    let chart;
+    getSubjects(userId, subjects_list, null);
     function generate() {
         const subject = subjects_list.value;
-        if (avgChart) avgChart.destroy();
+        if(!subject) return;
+        if (chart) chart.destroy();
         const xhr = new XMLHttpRequest();
         xhr.open('POST', '../api/api.php?get_action=getAttReport', true);
         xhr.setRequestHeader('Content-Type', 'application/json');
@@ -67,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function createChart(months, absents, presents) {
-        if (avgChart) avgChart.destroy();
+        if (chart) chart.destroy();
         const ctx = document.getElementById('average-grade-chart').getContext('2d');
         const canvas_container = document.querySelector('.canvas-container');
         const isMobile = window.innerWidth <= 768;
@@ -77,8 +47,8 @@ document.addEventListener("DOMContentLoaded", () => {
         ctx.hidden = false;
         const maxRotation = isMobile ? 90 : 0;
         const datasets = [];
-        avgChart = new Chart(ctx, {
-            type: 'bar', // Вы можете использовать 'bar', 'line', 'pie' и другие типы графиков
+        chart = new Chart(ctx, {
+            type: 'bar',
             data: {
                 labels: months,
                 datasets: [
@@ -117,44 +87,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             }
         });
-
-        createExportButton();
-    }
-
-    function createExportButton() {
-        let export_button = document.querySelectorAll('.export-container');
-        if (export_button.length > 0) export_button.forEach(button => { button.remove(); });
-        const div = document.createElement("div");
-        div.classList.add('export-container');
-        const input = document.createElement("input");
-        input.type = 'button';
-        input.value = 'Экспорт Excel';
-        input.classList.add('export');
-        div.appendChild(input);
-        document.body.appendChild(div);
-        div.addEventListener('click', () => {
-            const labels = avgChart.data.labels;
-            const datasets = avgChart.data.datasets;
-            const excelData = [];
-            const headers = [...datasets.map(dataset => dataset.label)];
-            // const headers = [avgChart.data.labels[0], ...avgChart.data.datasets.map(dataset => dataset.label)];
-            excelData.push(headers);
-
-            // Добавляем данные
-            labels.forEach((label, index) => {
-                const row = [label];
-                datasets.forEach(dataset => {
-                    row.push(dataset.data[index]);
-                });
-                excelData.push(row);
-            });
-
-            // Преобразуем данные в формат Excel
-            const ws = XLSX.utils.aoa_to_sheet(excelData); // Преобразование массива в лист
-            const wb = XLSX.utils.book_new(); // Создаем новую книгу
-            XLSX.utils.book_append_sheet(wb, ws, 'Chart Data'); // Добавляем лист в книгу
-            XLSX.writeFile(wb, 'chart_data.xlsx');
-        });
-
+        createExportButton(chart);
     }
 });
